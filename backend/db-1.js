d2e61bb084408f384967867174fc0180c0b9836b
rejneesh <pillairejneesh@gmail.com>
2019-07-13 23:00:29 +0530

    removed ng folder to create from start

---
+++ backend/db-1.js
@@ -0,0 +1,19 @@
+const mongodb = require('mongodb');
+const MongoClient = mongodb.MongoClient
+
+const connectionURL = "mongodb://127.0.0.1:27017"
+const databaseName = 'task-manager'
+
+MongoClient.connect(connectionURL, {
+    useNewUrlParser: true
+}, (error, client) => {
+    if (error) {
+        return console.log(error);
+    }
+    console.log('connected')
+    const db = client.db(databaseName)
+    db.collection('users').insertOne({
+        name: 'rejneesh',
+        age: 27
+    })
+})
